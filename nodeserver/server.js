2d8b31df38ee7b2bd1e9a69aceebc92960a2521a
Veiko Soomets <veiko.soomets@gmail.com>
2016-10-10 13:53:31 +0300

    Modified nodeserver file

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -2,21 +2,21 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var redis = require('redis');
- 
-server.listen(8890);
-var listener = io.listen(server);
-listener.sockets.on('connection', function (socket) {
- 
-  console.log("new client connected");
-  var redisClient = redis.createClient();
-  redisClient.subscribe('updates');
- 
-  redisClient.on("message", function(channel, message) {
-    console.log("new message in queue: "+ message);
-    socket.emit(channel, message);
-  });
- 
-  socket.on('disconnect', function() {
-    redisClient.quit();
+var port = 8890;
+
+server.listen(port, function(err){
+  io.on('connection', function(socket){   
+    console.log("new client connected");
+    var redisClient = redis.createClient();
+    redisClient.subscribe('updates');
+   
+    redisClient.on("message", function(channel, message) {
+      console.log("new message in queue: "+ message);
+      socket.emit(channel, message);
+    });
+   
+    socket.on('disconnect', function() {
+      redisClient.quit();
+    });
   });
 });
